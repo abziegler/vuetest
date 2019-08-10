848a0fa24dc587cf7c978c44ca1588523f5442e7
Adam Ziegler <abziegler@gmail.com>
2019-08-10 07:40:55 -0400

    content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="I VUE"/>
   </div>
 </template>
 
@@ -18,11 +18,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #773BCD;
   margin-top: 60px;
 }
 </style>
